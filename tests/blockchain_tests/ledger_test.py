1632b46559f4a40f9c24ba3c5b190a8b3709ebef
Gergely Tabiczky <tgergo@runbox.com>
2019-04-22 12:38:46 +0200

    adding ledger tests

--- tests/blockchain_tests/ledger_test.py
+++ tests/blockchain_tests/ledger_test.py
@@ -24,7 +24,7 @@ def root_block(good_transaction):
 
 
 class TestNewBlock:
-    def test_new_block(self):
+    def test_create_root_block(self):
         block = TransactionBlock.new_block()
         assert_that(block).is_type_of(TransactionBlock)
         assert_that(block).is_not_none()
@@ -33,11 +33,48 @@ class TestNewBlock:
         assert_that(block.previous_hash).is_equal_to(b'')
         assert_that(block.nonce).is_none()
 
+    def test_create_child_block(self, root_block):
+        block = TransactionBlock.new_block(previous_block=root_block)
+        assert_that(block).is_type_of(TransactionBlock)
+        assert_that(block).is_not_none()
+        assert_that(block).has_data(())
+        assert_that(block.previous_block).is_equal_to(root_block)
+        assert_that(block.previous_hash).is_equal_to(root_block.compute_hash())
+        assert_that(block.nonce).is_none()
+
+    def test_create_block_with_data(self, root_block):
+        block = TransactionBlock.new_block(('transaction', ), root_block)
+        assert_that(block).is_not_none()
+        assert_that(block).has_data(('transaction', ))
+        assert_that(block.previous_block).is_equal_to(root_block)
+        assert_that(block.previous_hash).is_equal_to(root_block.compute_hash())
+        assert_that(block.nonce).is_none()
+
 
 class TestAddTransaction:
-    def test_add_transactions(self, root_block, good_transaction):
+    def test_add_first_transaction(self, root_block):
         block = TransactionBlock.new_block(previous_block=root_block)
-        block = block.add_transaction(good_transaction)
-        assert_that(block.data).is_type_of(tuple)
-        assert_that(block.previous_hash).is_equal_to(block.previous_block.compute_hash())
-        assert_that(block.verify()).contains(VERIFIED)
+        block = block.add_transaction('transaction')
+        assert_that(block).has_data(('transaction', ))
+
+    def test_add_to_existing_transactions(self, root_block):
+        block = TransactionBlock.new_block(('trx1', ), root_block)
+        block = block.add_transaction('trx2')
+        assert_that(block).has_data(('trx1', 'trx2'))
+
+
+class TestAddTransactions:
+    def test_add_first_transactions(self, root_block):
+        block = TransactionBlock.new_block(previous_block=root_block)
+        block = block.add_transactions(('trx1', 'trx2'))
+        assert_that(block).has_data(('trx1', 'trx2'))
+
+    def test_add_to_existing_transactions(self, root_block):
+        block = TransactionBlock.new_block(('trx1', 'trx2'), root_block)
+        block = block.add_transactions(('trx3', 'trx4'))
+        assert_that(block).has_data(('trx1', 'trx2', 'trx3', 'trx4'))
+
+    def test_add_with_single_element_tuple(self, root_block):
+        block = TransactionBlock.new_block(('trx1', 'trx2'), root_block)
+        block = block.add_transactions(('trx3',))
+        assert_that(block).has_data(('trx1', 'trx2', 'trx3'))
